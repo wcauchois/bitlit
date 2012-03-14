4bc1f8663f61b179e7ce7e3c0961a1c803d27247
Bill Cauchois <wcauchois@gmail.com>
2012-03-13 18:46:21 -0700

    initial commit with rails scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bitlit::Application.config.secret_token = '5392f8e8a8047884a0e4908d6d2aee4c53155bf118f3e3ee32aa3cdaffec2484a95a40a7909075955395d9d5fa0e52a0c0fb5b7f767fe73fd56c74c9a5063a59'
